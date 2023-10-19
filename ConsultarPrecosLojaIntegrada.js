5bf9c8d5c1c5606d6a0ca6d38448b8a673977160
alefReis <alef.rdr2@gmail.com>
2023-10-19 02:33:15 -0300

    teste

--- ConsultarPrecosLojaIntegrada.js
+++ ConsultarPrecosLojaIntegrada.js
@@ -1,7 +1,7 @@
 /* ASM HydraSynth Deluxe 73 Teclas */
 $(function(){
-    $('.acoes-produto.disponivel.SKU-314 .comprar').prepend('<a href="https://wa.me/5511982602334?text=*Ol%C3%A1,%20pode%20me%20ajudar?%20Eu%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20para%20a%20compra%20do%20produto:*%0AValor:%20R$%2019.991,04%0AC%C3%B3digo:%20314%0Ahttps://www.classickeyboards.com.br/teclado-sintetizador-asm-hydrasynth-deluxe-73-teclas"  class="botao-secundario" rel="nofollow" data-placement="left" style=" display: flex; color: #ffffff; background-color: #d30000; font-size: 19px; font-weight: 700; border: 1px solid #d30000; border-radius: 10px; padding: 15px 50px; cursor: pointer; margin-bottom: 25%"><i class="icon-shopping-cart" style="margin-right: 5px;"></i>Consultar</a>');
+    $('.acoes-produto.disponivel.SKU-315 .comprar').prepend('<a href="https://wa.me/5511982602334?text=*Ol%C3%A1,%20pode%20me%20ajudar?%20Eu%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20para%20a%20compra%20do%20produto:*%0AValor:%20R$%2019.991,04%0AC%C3%B3digo:%20314%0Ahttps://www.classickeyboards.com.br/teclado-sintetizador-asm-hydrasynth-deluxe-73-teclas"  class="botao-secundario" rel="nofollow" data-placement="left" style=" display: flex; color: #ffffff; background-color: #d30000; font-size: 19px; font-weight: 700; border: 1px solid #d30000; border-radius: 10px; padding: 15px 50px; cursor: pointer; margin-bottom: 25%"><i class="icon-shopping-cart" style="margin-right: 5px;"></i>Consultar</a>');
 
-$('.acoes-produto.disponivel.SKU-314 .comprar').css('cssText', 'min-width:15%!important;max-width:15%!important;padding-left:0px');
+$('.acoes-produto.disponivel.SKU-315 .comprar').css('cssText', 'min-width:15%!important;max-width:15%!important;padding-left:0px');
 
 });
